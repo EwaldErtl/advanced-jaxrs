7b84b04a43b3965bb16ca6f446d1b14859c5953c
eertl <eertl@familiy>
2016-03-23 21:10:33 +0100

    Initial import after lesson 01 on javabrains.io for adavaned JAX-RS

---
+++ src/main/java/com/hartter/ewer/javabrain/advanced/rest/MyApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hartter.ewer.javabrain.advanced.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author eertl
+ */
+@ApplicationPath( "webapi")
+public class MyApp extends Application{
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet classes = new HashSet<>();
+        classes.add(MyResource.class);
+        
+        return classes;
+    }
+ 
+    
+}
